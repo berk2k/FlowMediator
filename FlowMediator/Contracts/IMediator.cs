c0b073cf03d65d6f2bc93161b13ff69a68f642b9
Berk Polat <berkpolat125@gmail.com>
2025-08-24 16:53:44 +0300

    mediator interface

---
+++ FlowMediator/Contracts/IMediator.cs
@@ -0,0 +1,11 @@
+﻿namespace FlowMediator.Contracts
+{
+    /// <summary>
+    /// The mediator that receives a request and dispatches it to the appropriate handler.
+    /// </summary>
+
+    public interface IMediator
+    {
+        TResponse Send<TResponse>(IRequest<TResponse> request);
+    }
+}
