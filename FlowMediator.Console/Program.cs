ee40aa8c874dc944c982ac7b2bc85d889eea2905
Berk Polat <berkpolat125@gmail.com>
2025-08-25 15:17:49 +0300

    testing pipeline behaviors logging and validation

--- FlowMediator.Console/Program.cs
+++ FlowMediator.Console/Program.cs
@@ -3,19 +3,37 @@ using FlowMediator.Contracts;
 using FlowMediator.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using FluentValidation;
 
 
 
 var services = new ServiceCollection();
 
-// register mediator + handlers + behaviors automatically
+
 services.AddFlowMediator(Assembly.GetExecutingAssembly());
+
 services.AddSingleton<IUserRepository, UserRepository>();
+
+services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
 var provider = services.BuildServiceProvider();
 var mediator = provider.GetRequiredService<IMediator>();
 
-// test: request via mediator
-var user = await mediator.SendAsync(new GetUserByIdQuery(1));
 
+var user = await mediator.SendAsync(new GetUserByIdQuery(1));
 Console.WriteLine($"User Id: {user.Id}, Name: {user.Name}");
+
+
+try
+{
+    var invalidUser = await mediator.SendAsync(new GetUserByIdQuery(0));
+    Console.WriteLine($"User Id: {invalidUser.Id}, Name: {invalidUser.Name}");
+}
+catch (ValidationException ex)
+{
+    Console.WriteLine("Validation failed:");
+    foreach (var error in ex.Errors)
+    {
+        Console.WriteLine($"   - {error.PropertyName}: {error.ErrorMessage}");
+    }
+}
---
+++ FlowMediator.Console/Validators/GetUserByIdQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace FlowMediator.Console.Validators
+{
+    public class GetUserByIdQueryValidator : AbstractValidator<GetUserByIdQuery>
+    {
+        public GetUserByIdQueryValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("User Id must be greater than 0");
+        }
+    }
+}
