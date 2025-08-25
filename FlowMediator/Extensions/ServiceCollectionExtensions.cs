4222099f7fc99115052b01fa088831fd34b40670
Berk Polat <berkpolat125@gmail.com>
2025-08-25 11:18:25 +0300

    extension method for adding handlers into DI container

---
+++ FlowMediator/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using FlowMediator.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace FlowMediator.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFlowMediator(this IServiceCollection services, Assembly assembly)
+        {
+            // Register all IRequestHandler<TRequest, TResponse>
+            var handlerTypes = assembly.GetTypes()
+                .Where(t => !t.IsAbstract && !t.IsInterface)
+                .SelectMany(t => t.GetInterfaces()
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>))
+                    .Select(i => new { Handler = t, Interface = i }));
+
+            foreach (var h in handlerTypes)
+            {
+                services.AddTransient(h.Interface, h.Handler);
+            }
+
+            // Register all IRequestHandlerAsync<TRequest, TResponse>
+            var asyncHandlerTypes = assembly.GetTypes()
+                .Where(t => !t.IsAbstract && !t.IsInterface)
+                .SelectMany(t => t.GetInterfaces()
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandlerAsync<,>))
+                    .Select(i => new { Handler = t, Interface = i }));
+
+            foreach (var h in asyncHandlerTypes)
+            {
+                services.AddTransient(h.Interface, h.Handler);
+            }
+
+            // Register mediator itself
+            services.AddSingleton<IMediator, Core.Mediator>();
+
+            return services;
+        }
+    }
+}
