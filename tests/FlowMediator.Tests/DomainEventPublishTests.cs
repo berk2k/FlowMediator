3463211eee11ba9819bf7138f03d8b272d6a26d9
Berk Polat <berkpolat125@gmail.com>
2026-01-26 12:22:57 +0300

    tested: v2 features

---
+++ tests/FlowMediator.Tests/DomainEventPublishTests.cs
@@ -0,0 +1,29 @@
+﻿using FlowMediator.Contracts;
+using FlowMediator.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowMediator.Tests
+{
+    public class DomainEventPublishTests
+    {
+        [Fact]
+        public async Task PublishAsync_Should_Invoke_EventHandlers()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddFlowMediator(typeof(DomainEventPublishTests).Assembly);
+
+            var provider = services.BuildServiceProvider();
+            var mediator = provider.GetRequiredService<IMediator>();
+
+            var testEvent = new TestEvent("hello");
+
+            // Act
+            await mediator.PublishAsync(testEvent);
+
+            // Assert
+            Assert.True(TestEventHandler.WasHandled);
+        }
+    }
+
+}
--- tests/FlowMediator.Tests/DomainEvents/TestEventHandler.cs
+++ tests/FlowMediator.Tests/DomainEvents/TestEventHandler.cs
@@ -1,14 +1,12 @@
 ﻿using FlowMediator.Contracts;
 
-namespace FlowMediator.Tests.DomainEvents
+public class TestEventHandler : IEventHandler<TestEvent>
 {
-    public class TestEventHandler : IRequestHandler<TestEvent, Unit>
-    {
-        public Task<Unit> Handle(TestEvent request, CancellationToken cancellationToken)
-        {
-            System.Console.WriteLine($"[Handler] Event triggered with message: {request.Message}");
-            return Task.FromResult(Unit.Value);
-        }
-    }
+    public static bool WasHandled { get; private set; }
 
+    public Task Handle(TestEvent @event, CancellationToken cancellationToken)
+    {
+        WasHandled = true;
+        return Task.CompletedTask;
+    }
 }
