50632f942a9b3578c9931cfaa338855a820fd1f1
Berk Polat <berkpolat125@gmail.com>
2026-01-26 12:34:30 +0300

    tested: demo

---
+++ FlowMediator.Console/EventHandlers/UserCreatedEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using FlowMediator.Contracts;
+using FlowMediator.Console.Events;
+
+namespace FlowMediator.Console.EventHandlers
+{
+    public class UserCreatedEventHandler
+        : IEventHandler<UserCreatedEvent>
+    {
+        public Task Handle(
+            UserCreatedEvent @event,
+            CancellationToken cancellationToken)
+        {
+            System.Console.WriteLine(
+                $"[EVENT] User created: {@event.UserName}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ FlowMediator.Console/Events/UserCreatedEvent.cs
@@ -0,0 +1,18 @@
+﻿using FlowMediator.Contracts;
+
+namespace FlowMediator.Console.Events
+{
+    public class UserCreatedEvent : IEvent
+    {
+        public Guid EventId { get; } = Guid.NewGuid();
+        public DateTime OccurredOn { get; } = DateTime.UtcNow;
+
+        public string UserName { get; }
+
+        public UserCreatedEvent(string userName)
+        {
+            UserName = userName
+                ?? throw new ArgumentNullException(nameof(userName));
+        }
+    }
+}
